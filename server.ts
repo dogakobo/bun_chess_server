5273e73908b2a716e9d97f3a9ec721d1230543b5
Antonio Quevedo <thecreed32@hotmail.com>
2025-05-28 21:44:57 -0600

    cors added

--- server.ts
+++ server.ts
@@ -28,7 +28,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000"
+    origin: process.env.CORS
   }
 });
 
