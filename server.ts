3b22eaee5f69a4913f3ae97b78bda4c328148a44
Antonio Quevedo <thecreed32@hotmail.com>
2025-06-01 19:32:58 -0600

    asd

--- server.ts
+++ server.ts
@@ -43,9 +43,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: allowedDomains,
-    methods: ["GET", "POST"],
-    credentials: true,
-    allowedHeaders: ["content-type"],
+    methods: ["GET", "POST"]
   }
 });
 
